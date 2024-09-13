993554d7eba9f4f8e7861c65ebf859f8af5b3dbd
José Sandro Rogério Mendes de Araújo <jsrma@dmz.org.br>
2024-09-13 17:04:03 -0300

    Including tests for viewmodel

---
+++ FareCalculator.Core/Common/EnumExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace FareCalculator.Core.Common
+{
+    public static  class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+
+        public static T GetValueFromDescription<T>(string description) where T : Enum
+        {
+            foreach (var field in typeof(T).GetFields())
+            {
+                DescriptionAttribute attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (attribute != null && attribute.Description == description)
+                    return (T)field.GetValue(null);
+
+                if (field.Name == description)
+                    return (T)field.GetValue(null);
+            }
+
+            throw new ArgumentException("Not found.", nameof(description));
+        }
+    }
+}
---
+++ FareCalculator.Tests/Mocks/FareCalculatorServiceMock.cs
@@ -0,0 +1,19 @@
+﻿using FareCalculator.Core.Models;
+using FareCalculator.Core.Services.Interfaces;
+using PCLMock;
+
+namespace FareCalculator.Tests.Mocks
+{
+    public class FareCalculatorServiceMock : MockBase<IFareCalculatorService>, IFareCalculatorService
+    {
+        public FareCalculatorServiceMock(MockBehavior behavior = MockBehavior.Loose)
+            : base(behavior)
+        {
+        }
+
+        public decimal CalculateFare(UberRideBase vehicle, int distance, decimal weight, decimal volume)
+        {
+            return this.Apply(x => x.CalculateFare(vehicle, distance, weight, volume));
+        }
+    }
+}
